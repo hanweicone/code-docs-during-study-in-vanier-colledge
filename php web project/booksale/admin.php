328fc750568d520ebc4611d4a82fa22a097076a3
hanweicone <48724844+hanweicone@users.noreply.github.com>
2019-12-20 02:09:31 -0500

    d

--- php web project/booksale/admin.php
+++
@@ -1,3 +0,0 @@
-<?php
-  echo '<script>location="admin/index.php"</script>';
-?>
--- php web project/booksale/home/cart/clear.php
+++
@@ -1,8 +0,0 @@
-<?php
-  session_start();
-  include '../../public/common/conn.php';
-
-  $_SESSION['books']=array();
-
-  echo '<script>location="index.php"</script>';
-?>
